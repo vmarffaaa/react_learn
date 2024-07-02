05cd790f1ecbf4348bf0cb01b6ff447ab903ba5f
Marta <veeenling@gmail.com>
2024-07-02 19:05:32 +0300

    finally css for useToogle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { useToggle } from './hooks/CustomHooks';
 import './App.css'
+import styles from './hooks/Toogler.module.css'
 
 function App() {
   const [isSidebarOpen, toggleSidebar] = useToggle();
 
   return (
-      <div>
-          <h1>Toggle Button</h1>
-        <button
-            className={isSidebarOpen ? 'true' : 'false'}
-            onClick={toggleSidebar}>
-          {isSidebarOpen ? "True" : "False"}
-        </button>
-      </div>
+      <div className={styles.flex}>
+          <div className={styles.toogler}>
+              <h1>Toggle Button</h1>
+            <button
+                className={isSidebarOpen ? 'true' : 'false'}
+                onClick={toggleSidebar}>
+              {isSidebarOpen ? "True" : "False"}
+            </button>
+          </div>
+    </div>
   );
 }
 
