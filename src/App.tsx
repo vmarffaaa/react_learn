64dfabddb4b85d30666fd045d5c58a6dde316c2d
Marta <veeenling@gmail.com>
2024-07-03 19:55:17 +0300

    finally hook usePrevios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {usePrevious} from './hooks/UsePrevios'
 import './App.css';
+import styles from './hooks/UsePrevios.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Counter = () => {
+    const [value, setValue] = useState<number>(0);
+    const previousValue = usePrevious(value);
+
+    return (
+        <div className={styles.box}>
+            <div className={styles.flex}>
+                <h2 className={styles.current_value}>Current value: {value}</h2>
+                <h2 className={styles.previous_value}>Previous value: {previousValue}</h2>
+            </div>
+            <button
+                className={styles.increment}
+                onClick={() => {
+                    setValue((prevState) => prevState + 1);
+                }}
+            >
+                Increment
+            </button>
+        </div>
+    );
 }
 
-export default App;
+export default Counter;
